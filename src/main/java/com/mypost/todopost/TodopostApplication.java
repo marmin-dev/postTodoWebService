b5f93caa8654cea4836419cd3d1af5fb27454148
Marmin <marmin.dev@gmail.com>
2022-12-23 00:13:58 +0900

    StartProject

---
+++ src/main/java/com/mypost/todopost/TodopostApplication.java
@@ -0,0 +1,13 @@
+package com.mypost.todopost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodopostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodopostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypost/todopost/TodopostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypost.todopost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodopostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
